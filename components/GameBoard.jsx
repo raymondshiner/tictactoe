7d26d7323baf8ffe65b87fd3633a3ef0d4cd574d
Raymond Shiner <rshiner@A-1087.local>
2022-04-06 12:32:36 -0700

    get gameboard rendering

--- components/GameBoard.jsx
+++ components/GameBoard.jsx
@@ -1,5 +1,30 @@
+import styled from "styled-components"
+
 const GameBoard = () => {
-  return <div>Im a GameBoard</div>
+  return (
+    <BackgroundLines>
+      {[...Array(9)].map((val, index) => {
+        return <GameSquare key={val} />
+      })}
+    </BackgroundLines>
+  )
 }
+
+const emptySquareSize = "70px"
+
+const BackgroundLines = styled.div`
+  display: grid;
+  height: min-content;
+  background-color: black;
+  grid-template-columns: repeat(3, ${emptySquareSize});
+  grid-template-rows: repeat(3, ${emptySquareSize});
+  gap: 10px;
+`
+
+const GameSquare = styled.div`
+  width: ${emptySquareSize};
+  height: 100%;
+  background-color: white;
+`
 
 export default GameBoard
